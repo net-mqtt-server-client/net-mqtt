d16e50b711eb2f55c166c4b82e68e7137afdd996
evgenius081 <gulevichzhenya@gmail.com>
2023-11-02 20:33:19 +0100

    fix: library imports fixed

---
+++ SIN.Infrastructure/Repositories/MeasurementRepository.cs
@@ -0,0 +1,94 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using SIN.Domain.Entities;
+using SIN.Infrastructure.Context.Interfaces;
+using SIN.Infrastructure.Repositories.Interfaces;
+
+namespace SIN.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Repository implementing <see cref="IMeasurementRepository"/>.
+    /// </summary>
+    public class MeasurementRepository : IMeasurementRepository
+    {
+        private readonly IApplicationContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MeasurementRepository"/> class.
+        /// </summary>
+        /// <param name="context">Application context.</param>
+        public MeasurementRepository(IApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<Measurement>> GetByFiltersAsync(string location, string sensor, string orderBy, string order, string number)
+        {
+            var measurements = this.context.Measurements.AsQueryable();
+            if (!string.IsNullOrEmpty(location))
+            {
+                measurements = measurements.Where(m => m.Location == location);
+            }
+
+            if (!string.IsNullOrEmpty(sensor))
+            {
+                measurements = measurements.Where(m => m.Sensor == sensor);
+            }
+
+            if (!string.IsNullOrEmpty(orderBy))
+            {
+                if (string.IsNullOrEmpty(order) || order == "asc")
+                {
+                    if (orderBy == "location")
+                    {
+                        measurements = measurements.OrderBy(m => m.Location);
+                    }
+                    else if (orderBy == "sensor")
+                    {
+                        measurements = measurements.OrderBy(m => m.Sensor);
+                    }
+                    else if (orderBy == "value")
+                    {
+                        measurements = measurements.OrderBy(m => m.Value);
+                    }
+                }
+                else if (order == "desc")
+                {
+                    if (orderBy == "location")
+                    {
+                        measurements = measurements.OrderByDescending(m => m.Location);
+                    }
+                    else if (orderBy == "sensor")
+                    {
+                        measurements = measurements.OrderByDescending(m => m.Sensor);
+                    }
+                    else if (orderBy == "value")
+                    {
+                        measurements = measurements.OrderByDescending(m => m.Value);
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(number))
+            {
+                int.TryParse(number, out int num);
+                measurements = measurements.Take(num);
+            }
+
+            return await measurements.ToListAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task<Measurement> GetByIdAsync(Guid id)
+        {
+            return await this.context.Measurements.Find(m => m.Id == id).FirstOrDefaultAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task SaveMeasurementAsync(Measurement measurement)
+        {
+            await this.context.Measurements.InsertOneAsync(measurement);
+        }
+    }
+}
---
+++ SIN/Hubs/HubClient.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SIN.Hubs
+{
+    /// <summary>
+    /// SignalR client.
+    /// </summary>
+    public class HubClient : Hub
+    {
+        /// <summary>
+        /// Logger.
+        /// </summary>
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HubClient"/> class.
+        /// </summary>
+        /// <param name="logger">Logger object.</param>
+        public HubClient(ILogger<HubClient> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public override async Task OnConnectedAsync()
+        {
+            await Task.Run(() => this.logger.LogInformation($"Client ${this.Context.ConnectionId} connected."));
+        }
+    }
+}
