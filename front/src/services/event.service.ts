b5b181c0279da884625d8787de0321521eea960a
Dar-Na <denisdurd13@gmail.com>
2023-11-02 20:25:25 +0100

    front - event bus and event service created

---
+++ front/src/services/event.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Subject } from 'rxjs/internal/Subject';
+import { filter, map } from 'rxjs/operators';
+
+export enum Events {
+  FILTER,
+  LOGOUT,
+}
+
+interface BroadcastEvent {
+  key: any;
+  data?: any;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+  private _eventBus: Subject<BroadcastEvent>;
+
+  constructor() {
+    this._eventBus = new Subject<BroadcastEvent>();
+  }
+
+  broadcast(key: any, data?: any) {
+    this._eventBus.next({ key, data });
+  }
+
+  on<T>(key: any): Observable<T> {
+    return this._eventBus.asObservable().pipe(
+      filter((event: any) => event.key === key),
+      map((event: any) => <T>event.data)
+    );
+  }
+}
